484dd41fef7ce9b32cda568db9c6c66251d3bdcf
hafizadityarozzaqy <30432180+hafizadityarozzaqy@users.noreply.github.com>
2017-10-17 09:59:25 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Register</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="form">
+	<div class="content">
+		<h1>Istagram</h1>
+		<h3>Sign up to see photos and videos<br>from your fiends</h3>
+		<button>Login with Facebook</button><br>
+		<p style="margin: 5px; color: #999">OR</p>
+		<input type="text" name="email" placeholder="Mobile Number or Email"><br>
+		<input type="text" name="full-name" placeholder="Full Name"><br>
+		<input type="text" name="username" placeholder="Uername"><br>
+		<input type="password" name="password" placeholder="Password"><br>
+		<button type="submit">Sign Up</button><br>
+		<p>By signing up, you agree to our<br><b>Terms & Privacy Policy</b>.</p>
+	</div>
+</center>
+</div>
+<center>
+
+</body>
+</html>
